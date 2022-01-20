6a0aa24fb0b1217daedc29d104528781d1f400a5
DawidPlociennikDev <dawid.plociennik13@gmail.com>
2022-01-20 14:55:05 +0100

    test time send email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -9,18 +9,19 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Rap2hpoutre\FastExcel\FastExcel;
 
 class EmailController extends Controller
 {
     public function sendEmail()
     {
-        $emails_address = [
-            0 => 'd.plociennik@adawards.pl',
-            // 1 => 'k.zygadlo@adawards.pl',
-            // 2 => 'd.lewicki@adawards.pl',
-            // 3 => '4p.koper@adawards.pl',
-        ];
-
+        $emails_address = [];
+        // plik CSV z 270 tys. rekordów (przy 50 000 się wywala skrypt (timeout max 60s))
+        foreach ($this->fetchCSV('emails270k.csv') as $key => $value) {
+            array_push($emails_address, $value['email']);
+            // podzielenie CSVki
+            if ($key >= 9) break;
+        }
         foreach ($emails_address as $key => $value) {
             try {
                 ProcessNewsletter::dispatch($key, $value)->delay(now()->addSeconds(2));
@@ -63,6 +64,15 @@ class EmailController extends Controller
                 Log::notice('Błąd anulowania subskrypcji użytkownikowi ' . $getUser->email . ' z tokenem ' . $request->userToken);
                 return;
             }
+        }
+    }
+
+    public function fetchCSV(string $filename)
+    {   
+        try {
+            return (new FastExcel)->import('csv/'.$filename);
+        } catch (\Exception $e) {
+            dd($e);
         }
     }
 }
--- resources/views/emails/newsletter_template.blade.php
+++ resources/views/emails/newsletter_template.blade.php
@@ -1,13 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AD Awards - Newsletter</title>
-    <link href="{{ asset('templates/assets/dist/css/bootstrap.min.css') }}" rel="stylesheet" />
-</head>
-<body>
-    <a href="{{ url('anuluj-subskrypcje/'.$userData['token']) }}" class="btn btn-primary">Anuluj subskrypcje Newslettera</a>
-</body>
-</html>l
+<div style="max-width: 75%; text-align: center; padding: 20px 30px; border: 1px solid #000; margin: auto">
+    
+    {{-- LOGO --}}
+    <img src="{{ asset('assets/img/logo.png') }}" alt="" width="128">
+    {{-- LOGO --}}
+
+    <br><br>
+    <hr><br>
+
+    {{-- TREŚĆ MAILA --}}
+    <div style="text-align: justify;">
+        <h3>Lorem ipsum</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero expedita soluta optio suscipit, iusto quod
+        voluptas fuga minima sunt deleniti est natus rem delectus obcaecati praesentium accusamus, aliquam dolore
+        aperiam!
+        <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloremque dolorem expedita modi iste vel
+        impedit voluptates magni corrupti dolor, eaque soluta dolore saepe. Suscipit voluptatibus amet soluta optio at.
+        <br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde inventore explicabo aliquid voluptatum
+        architecto! Itaque impedit eum quod nisi natus accusantium esse atque magni, consectetur tenetur pariatur quo
+        asperiores. Quidem.
+    </div>
+    {{-- TREŚĆ MAILA --}}
+
+    <br><br>
+    <hr><br><br>
+    <a href="{{ url('anuluj-subskrypcje/' . $userData['token']) }}" style="background: darkblue; 
+                color: #fff; 
+                padding: 10px 20px; 
+                border-radius: 3px;
+                margin-bottom: 20px;
+                text-decoration: none;">
+        Anuluj subskrypcje Newslettera
+    </a>
+</div>
